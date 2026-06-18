859c407d3e7c98d7e85a20f2f06fbed56d3546e3
Corey Lockard <crlboy94@gmail.com>
2026-06-18 16:29:08 -0400

    Added delete button and wired it up

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,9 +1,49 @@
-export default function ActivityList({ activities }) {
+import { deleteActivity } from "../api/activities";
+import { useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+
+//instead of building the li here it calls activityEntry
+export default function ActivityList({ activities, syncActivities }) {
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
+        <ActivityEntry
+          key={activity.id}
+          activity={activity}
+          syncActivities={syncActivities}
+        />
       ))}
     </ul>
+  );
+}
+
+//this calls useauth to get the token data so it can be used
+//creates an error that can be set within the function
+//and creates delete functionality that sets the error to null
+//then calls the delete function wired up to the API
+//finally it re-syncs the activities so they update
+//if any errors are thrown they are then displayed.
+function ActivityEntry({ activity, syncActivities }) {
+  const { token } = useAuth();
+
+  const [error, setError] = useState(null);
+
+  const handleDelete = async () => {
+    setError(null);
+
+    try {
+      await deleteActivity(activity.id, token);
+      syncActivities();
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <li>
+      <p>{activity.name}</p>
+      {token && <button onClick={handleDelete}>Delete Activity</button>}
+      {error && <p>{error}</p>}
+    </li>
   );
 }
--- src/api/activities.js
+++ src/api/activities.js
@@ -35,3 +35,19 @@ export async function createActivity(token, activity) {
     throw Error(result.message);
   }
 }
+
+export async function deleteActivity(id, token) {
+  if (!token) {
+    throw Error("You must sign in to delete an activity");
+  }
+
+  const response = await fetch(API + "/activities/" + id, {
+    method: "DELETE",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
